8ae2f92fc52c81a6cee2928c29d31df25e9e7e39
Bhanu Vinzamuri <bhanu.vinzamuri@ibm.com>
2020-01-03 20:55:37 -0500

    VAE with interpolation

--- aix360/algorithms/dipvae/__init__.py
+++ aix360/algorithms/dipvae/__init__.py
@@ -1,2 +1,2 @@
-from .dipvae import DIPVAEExplainer
-from .dipvae_utils import VAE, DIPVAE, plot_latent_traversal, plot_reconstructions
+from .dipvae_v2 import DIPVAEExplainer
+from .dipvae_utils_v2 import VAE, DIPVAE, plot_latent_traversal, plot_reconstructions
